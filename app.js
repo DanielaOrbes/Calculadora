4d99fd1161b6b0a52987c403e7737966cf94b5e0
DanielaOrbes <daniiorbes15@gmail.com>
2022-09-06 14:17:08 -0300

    calculadora completa

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const process = require ('process');
 const sumar = require('./sumar');
 const restar = require('./restar');
 const multiplicar = require('./multiplicar');
 const dividir = require('./dividir');
 
-console.log(sumar(8,5));
-console.log(restar(8,5));
-console.log(multiplicar(8,0));
-console.log(dividir(8,0));
+
+const operacion = process.argv[2];
+const numero1 = +process.argv[3]
+const numero2 = +process.argv[4]
+
+if(process.argv[3] == undefined || process.argv[4] == undefined){
+    return console.log('Ingrese ambos números');
+}else{
+switch (operacion){
+    case 'sumar':
+        console.log(sumar(numero1,numero2));
+         break;
+    case 'restar':
+        console.log(restar(numero1,numero2));
+         break;
+    case 'dividir':
+         console.log(dividir(numero1,numero2));
+         break;
+    case 'multiplicar':
+         console.log(multiplicar(numero1,numero2));
+         break;
+    default:
+        console.log('La operación '+ process.argv[2] + ' ingresada no es válida!');
+    }
+}
